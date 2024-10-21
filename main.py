94b54cd68856707d67ac9430039d1da1b78b2b8b
trebelatto <trebelatto@hawk.iit.edu>
2024-10-20 23:19:43 -0400

    base

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import mysql.connector
+import pyfiglet
+
+# Database connection
+host = "localhost"
+user = "root"
+password = ""
+database = ""
+
+try:
+    # Connect to mysql
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    if connection.is_connected():
+        print("Connected successfully")
+
+    cursor = connection.cursor()
+
+    # read data queries
 
