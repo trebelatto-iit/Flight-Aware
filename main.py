64ebc5ab11eed1c683dc889179ae2c95594b387d
trebelatto <trebelatto@hawk.iit.edu>
2024-10-22 14:45:38 -0400

    some read functions

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pyfiglet
 # Database connection
 host = "localhost"
 user = "root"
-password = ""
-database = ""
+password = "admintomas17"
+database = "flight_aware"
 
 try:
     # Connect to mysql
@@ -22,6 +22,35 @@ try:
     cursor = connection.cursor()
 
     # read data queries
+
+    ## read plane table
+    def read_data_plane():
+        select_query = "SELECT PlaneID, PlaneModel, Airline FROM Plane;"
+        cursor.execute(select_query)
+        planes = cursor.fetchall()
+        print("Plane Data:")
+        for plane in planes:
+            print(f"PlaneID: {plane[0]} | PlaneModel: {plane[1]} | Airline: {plane[2]}")
+
+    ## read airline table
+    def read_data_airline():
+        select_query = ""
+        cursor.execute(select_query)
+
+    ## read flight table
+    def read_data_flight():
+        select_query = ""
+        cursor.execute(select_query)
+
+    ## read airport table
+    def read_data_airport():
+        select_query = ""
+        cursor.execute(select_query)
+
+
+
+
+
 
 except mysql.connector.Error as error:
     print("Error connecting to MySQL:", error)
