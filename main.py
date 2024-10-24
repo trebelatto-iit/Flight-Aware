469f07b6de97fc79dae0c762b45edb4b85139711
trebelatto <trebelatto@hawk.iit.edu>
2024-10-24 14:09:57 -0400

    read functions complete

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ try:
         print("Plane Data:")
         for plane in planes:
             print(f"Plane ID: {plane[0]} | Plane Model: {plane[1]} | Airline: {plane[2]}")
+    
+    #read_data_plane()
 
     ## read airline table
     def read_data_airline():
@@ -39,6 +41,7 @@ try:
         print("Airline Data:")
         for airline in airlines:
             print(f"Airline Name: {airline[0]} | Airline Code: {airline[1]} | Country: {airline[2]}")
+    #read_data_airline()
 
     ## read flight table
     def read_data_flight():
@@ -47,35 +50,38 @@ try:
         flights = cursor.fetchall()
         print("Flight Data:")
         for flight in flights:
-            print(f"Flight Number: {flight[1]} | Airline ID: {flight[0]} | Plane ID: {flight[2]} | Departing: {flight[3]} | Arriving: {flight[4]} | Date: {flight[5]} | Status: {flight[6]}")
+            print(f"Flight Number: {flight[0]} | Airline ID: {flight[1]} | Plane ID: {flight[2]} | Departing: {flight[3]} | Arriving: {flight[4]} | Date: {flight[5]} | Status: {flight[6]}")
+    #read_data_flight()
 
     ## read airport table
     def read_data_airport():
-        select_query = ""
+        select_query = "SELECT AirportName, AirportCode, Location FROM Airport;"
         cursor.execute(select_query)
         airports = cursor.fetchall()
         print("Airport Data:")
         for airport in airports:
             print(f"Aiport Name: {airport[0]} | Airport Code: {airport[1]} | Location: {airport[2]}")
-
+    #read_data_airport()
 
     ## read passenger table
     def read_data_passenger():
-        select_query = ""
+        select_query = "SELECT LastName, FirstName, PassportID FROM Passenger;"
         cursor.execute(select_query)
         passengers = cursor.fetchall()
         print("Passenger Data:")
         for passenger in passengers:
-            print(f"First Name: {passenger[1]} | Last Name: {passenger[0]} | Passport ID: {passenger[2]}")
+            print(f"Name: {passenger[1]} {passenger[0]} | Passport ID: {passenger[2]}")
+    #read_data_passenger()
 
     ## read booking table
     def read_data_booking():
-        select_query = ""
+        select_query = "SELECT PassengerID, FlightID, SeatNumber, Date FROM Booking;"
         cursor.execute(select_query)
         bookings = cursor.fetchall()
         print("Booking Data:")
         for booking in bookings:
             print(f"Passenger ID: {booking[0]} | Flight ID: {booking[1]} | Seat Number {booking[2]} | Date: {booking[3]}")
+    #read_data_booking()
 
 ## more advanced read queries
 
@@ -101,7 +107,6 @@ try:
 
                 ## determine what to do with given choice using above functions
            
-    read_data_airport()
 
 except mysql.connector.Error as error:
     print("Error connecting to MySQL:", error)
